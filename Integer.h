c0d6f543b2896360b9058946bb07cc442a9179cf
Jishen Li <ljs765@gmail.com>
2014-07-07 12:28:53 -0500

    update

--- Integer.h
+++ Integer.h
@@ -16,6 +16,7 @@
 #include <stdexcept> // invalid_argument
 #include <string>    // string
 #include <vector>    // vector
+    #include <algorithm> // equal
 using namespace std;
 // -----------------
 // shift_left_digits
